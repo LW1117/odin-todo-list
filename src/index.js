959338fe6b92353ff6932359afcdd18f86f7baa4
Aslam <93756533+LW1117@users.noreply.github.com>
2024-11-14 16:47:20 +0530

    update files in template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import "./styles.css";
-import { home } from "./home";
-import { menu } from "./menu";
-import { about } from "./about";
-
-const homeButton = document.getElementById("home");
-const menuButton = document.getElementById("menu");
-const aboutButton = document.getElementById("about");
-
-homeButton.addEventListener("click", () => home());
-menuButton.addEventListener("click", () => menu());
-aboutButton.addEventListener("click", () => about());
-
-home();
